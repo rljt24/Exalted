b8341e2f173dd819ae6563dcb5bdea1c1c83a5c9
Ricky <ricky@ricky.com>
2023-07-31 20:21:38 -0500

    made damage button show variables in form in the console log

--- solarCharacterPage/src/components/HealthButtons.tsx
+++ solarCharacterPage/src/components/HealthButtons.tsx
@@ -1,28 +1,50 @@
-import {useState} from "react";
+import { useState } from "react";
 
 type HealthLevel = {
-    healthLevel: string,
-    damageType: string[]
-}
+  healthLevel: string;
+  damageType: string[];
+};
 const typesOfDamage: string[] = ["Bashing", "Leathal", "Agrivated"];
 
 function HealthButton({ levelsOfHealth }: { levelsOfHealth: HealthLevel[] }) {
-    const [updateLevelsOfHealth, setUpdateLevelsOfHealth] = useState({levelsOfHealth})
+  const [updateLevelsOfHealth, setUpdateLevelsOfHealth] = useState({
+    levelsOfHealth,
+  });
+  const [damageType, setDamageType] = useState(typesOfDamage[0]);
+  const [damageAmount, setDamageAmount] = useState(0);
+
+  const handleDamage = (this2: string, that: number) => {
+    console.log(this2 + " & " + that);
+  };
+
   return (
     <div>
-        <form>
-            <label>Select a Damage Type</label>
-            <select>
-            {typesOfDamage.map((td) => (
-               <option value={td}>{td}</option> 
-            ))}
-            </select><br></br>
-            <label>Amount of damage</label>
-            <input type="number"></input><br></br>
-            <button type="submit">Enter</button>
-        </form>
+      <form>
+        <label>Select a Damage Type</label>
+        <select onChange={(e) => setDamageType(e.target.value)}>
+          {typesOfDamage.map((td) => (
+            <option value={td}>{td}</option>
+          ))}
+        </select>
+        <br></br>
+        <label>Amount of damage</label>
+        <input
+          type="number"
+          value={damageAmount}
+          onChange={(e) => setDamageAmount(parseInt(e.target.value))}
+        ></input>
+        <br></br>
+        <button
+          type="button"
+          onClick={() => {
+            handleDamage(damageType, damageAmount);
+          }}
+        >
+          Ouch
+        </button>
+      </form>
     </div>
   );
-};
+}
 
 export default HealthButton;

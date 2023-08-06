78e01bd8899281b7a28e6b61417b180d73235b4f
Ricky <ricky@ricky.com>
2023-08-06 12:55:50 -0500

    lethal and aggravated damage working. need to fix spacing of damage levels. also need to clean up code for damage handler

--- solarCharacterPage/src/components/HealthButtons.tsx
+++ solarCharacterPage/src/components/HealthButtons.tsx
@@ -29,47 +29,77 @@ function HealthButton({ levelsOfHealth, updateLevelsOfHealth }: HealthProps) {
     e.preventDefault;
     let updatedData: HealthLevel[] = newData;
     let count: number = 0;
-    if(dT === damageTypes[1]){
-      outerloop:while(count < dA){
-        for(let i=0; i<updatedData.length;i++){
-          for(let j=0; j<updatedData[i].damageTypePerLevel.length; j++){
-            if(updatedData[i].damageTypePerLevel[j] === damageTypes[0]){
+    if (dT === damageTypes[1]) {
+      if(count < dA) {
+        outerloop:for (let i = 0; i < updatedData.length; i++) {
+          for (let j = 0; j < updatedData[i].damageTypePerLevel.length; j++) {
+            if (updatedData[i].damageTypePerLevel[j] === damageTypes[0]) {
               updatedData[i].damageTypePerLevel[j] = dT;
               count++;
-              if(count === dA){
+              if (count === dA) {
                 break outerloop;
               }
-              if(updatedData[i].healthLevel === '-4'){
+              if (updatedData[i].healthLevel === "-4") {
                 break outerloop;
               }
             }
           }
         }
       }
-      outerloop2:while(count < dA){
-        for(let k=0; k < updatedData.length; k++){
-          for(let x=0; x < updatedData[k].damageTypePerLevel.length; x++){
-            if(updatedData[k].damageTypePerLevel[x] === damageTypes[0] || updatedData[k].damageTypePerLevel[x] == damageTypes[1]){
+      if(count < dA) {
+        outerloop2:for (let k = 0; k < updatedData.length; k++) {
+          for (let x = 0; x < updatedData[k].damageTypePerLevel.length; x++) {
+            if (
+              updatedData[k].damageTypePerLevel[x] === damageTypes[0] ||
+              updatedData[k].damageTypePerLevel[x] == damageTypes[1]
+            ) {
               updatedData[k].damageTypePerLevel[x] = damageTypes[2];
               count++;
-              if(count === dA){
+              if (count === dA) {
                 break outerloop2;
               }
-              if(updatedData[k].healthLevel === '-4'){
+              if (updatedData[k].healthLevel === "-4") {
                 break outerloop2;
               }
             }
           }
         }
       }
-      lastLoop:while(count<dA){
-        if(dA-count === 1){
+      if(count < dA) {
+        if (dA - count === 1) {
           updatedData[4].damageTypePerLevel[0] = damageTypes[1];
-          break lastLoop;
         }
-        if(dA-count >= 2){
+        if (dA - count >= 2) {
           updatedData[4].damageTypePerLevel[0] = damageTypes[2];
-          break lastLoop;
+        }
+      }
+    } else if(dT === damageTypes[2]){
+      outerloop3:for(let y=0; y<updatedData.length; y++){
+        for(let z=0; z<updatedData[y].damageTypePerLevel.length; z++){
+          if (
+            updatedData[y].damageTypePerLevel[z] === damageTypes[0] ||
+            updatedData[y].damageTypePerLevel[z] == damageTypes[1]
+          ) {
+            updatedData[y].damageTypePerLevel[z] = damageTypes[2];
+            count++;
+            if (count === dA) {
+              break outerloop3;
+            }
+          }
+        }
+      }
+    } else if(dT === damageTypes[3]){
+      outerloop4:for(let a=0; a<updatedData.length; a++){
+        for(let b=0; b<updatedData[a].damageTypePerLevel.length; b++){
+          if (
+            updatedData[a].damageTypePerLevel[b] !== damageTypes[3]
+          ) {
+            updatedData[a].damageTypePerLevel[b] = damageTypes[3];
+            count++;
+            if (count === dA) {
+              break outerloop4;
+            }
+          }
         }
       }
     }
